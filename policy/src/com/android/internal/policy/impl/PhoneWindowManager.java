540a92a2a4cce815a13260a26a34b29780858ff1
Andy Mast <andy@cyngn.com>
2013-12-09 15:45:33 -0800

    Remove T-mobile items not used by new theme engine
    Change-Id: I1e9c14e63ec25fdff61cfd2b7b8a05b1e488e558
    
    Conflicts:
    	core/jni/AndroidRuntime.cpp
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    	services/java/com/android/server/AssetRedirectionManagerService.java
    
    Conflicts:
    	core/jni/AndroidRuntime.cpp
    
    Conflicts:
    
    	core/jni/AndroidRuntime.cpp
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    	services/java/com/android/server/pm/PackageManagerService.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -211,7 +211,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private final Object mLock = new Object();
 
     Context mContext;
-    Context mUiContext;
     IWindowManager mWindowManager;
     WindowManagerFuncs mWindowManagerFuncs;
     PowerManager mPowerManager;
@@ -943,6 +942,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         @Override
         public void run() {
             performScreenRecord();
+        }
+    };
+
+    private final Runnable mRingerChordLongPress = new Runnable() {
+        public void run() {
+            // Do the switch
+            final AudioManager am = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
+            final VolumePanel volumePanel = new VolumePanel(mContext,
+                                                              (AudioService) getAudioService());
+            volumePanel.postVolumeChanged(AudioManager.STREAM_RING,AudioManager.FLAG_SHOW_UI);
         }
     };
 
@@ -5003,12 +5012,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     updateSystemUiVisibilityLw();
                 }
             }
-        }
-    };
-
-    BroadcastReceiver mThemeChangeReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            mUiContext = null;
         }
     };
 
