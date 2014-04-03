284cc2dae7cdb763ffec66cff241ece93c870a9f
neophite31 <hard.d.25@gmail.com>
2014-04-03 11:06:20 +0200

    Revert "[1/2] Base: implement App circle sidebar"
    This reverts commit 98e933c3426f96d2565ef0076d7c1fb420e7e992.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1050,7 +1050,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     return false;
                 }
             });
-            addAppCircleSidebar();
         }
 
         // Setup pie container if enabled
