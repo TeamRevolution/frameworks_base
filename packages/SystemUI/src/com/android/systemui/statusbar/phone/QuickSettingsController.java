dskim9752/android_frameworks_base

Internal Memory Check Quick Panel

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -30,6 +30,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_LOCKSCREEN;
 import static com.android.internal.util.cm.QSConstants.TILE_LTE;
 import static com.android.internal.util.cm.QSConstants.TILE_MOBILEDATA;
 import static com.android.internal.util.cm.QSConstants.TILE_MUSIC;
+import static com.android.internal.util.cm.QSConstants.TILE_INTERNALMEMORY;
 import static com.android.internal.util.cm.QSConstants.TILE_NETWORKADB;
 import static com.android.internal.util.cm.QSConstants.TILE_NETWORKMODE;
 import static com.android.internal.util.cm.QSConstants.TILE_NFC;
@@ -80,6 +81,7 @@ import com.android.systemui.quicksettings.LteTile;
 import com.android.systemui.quicksettings.MobileNetworkTile;
 import com.android.systemui.quicksettings.MobileNetworkTypeTile;
 import com.android.systemui.quicksettings.MusicTile;
+import com.android.systemui.quicksettings.InternalMemory;
 import com.android.systemui.quicksettings.NetworkAdbTile;
 import com.android.systemui.quicksettings.NfcTile;
 import com.android.systemui.quicksettings.PerformanceProfileTile;
@@ -279,6 +281,8 @@ public class QuickSettingsController {
                 }
             } else if (tile.equals(TILE_MUSIC)) {
                 qs = new MusicTile(mContext, this);
+            } else if (tile.equals(TILE_INTERNALMEMORY)) {
+                qs = new InternalMemory(mContext, this);
             }
 
             if (qs != null) {

TeamRevolution/frameworks_base

Conflicts:
	core/java/com/android/internal/util/cm/QSConstants.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
	core/java/com/android/internal/util/slim/QSConstants.java
Change-Id: I45cca28f31f97aa86b268e39a833ba5a49b47e32

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -53,7 +53,12 @@ import static com.android.internal.util.slim.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFI;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
+import static com.android.internal.util.slim.QSConstants.TILE_INTERNALMEMORY;
 
+import android.app.Activity;
+import android.app.ActivityManagerNative;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -87,6 +92,7 @@ import com.android.systemui.quicksettings.LteTile;
 import com.android.systemui.quicksettings.MobileNetworkTile;
 import com.android.systemui.quicksettings.MobileNetworkTypeTile;
 import com.android.systemui.quicksettings.MusicTile;
+import com.android.systemui.quicksettings.InternalMemory;
 import com.android.systemui.quicksettings.NfcTile;
 import com.android.systemui.quicksettings.PreferencesTile;
 import com.android.systemui.quicksettings.QuickSettingsTile;
@@ -255,6 +261,8 @@ public class QuickSettingsController {
                 qs = new CustomTile(mContext, this, findCustomKey(tile));
             } else if (tile.contains(TILE_CONTACT)) {
                 qs = new ContactTile(mContext, this, findCustomKey(tile));
+            } else if (tile.equals(TILE_INTERNALMEMORY)) {
+                qs = new InternalMemory(mContext, this);
             }
 
             if (qs != null) {