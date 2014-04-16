DirtyUnicorns/android_frameworks_base

On-The-Go mode [1/2]
On-The-Go turns on the camera of your phone and streams it to an overlay,
which is drawn over everything.
this makes you feel like your display is transparent, as you see the content
behind it.

its really useful when you are using your phone while walking, thats why
we called it "On-The-Go"

allows to change transparency of the overlay via the notification.

have fun!

Change-Id: I3efccb5fac41a946f6cb4992e9181422980ee5e4

packages/SystemUI/src/com/android/systemui/nameless/onthego/OnTheGoDialog.java
@@ -1,0 +1,126 @@
+/*
+ * Copyright (C) 2014 The NamelessRom Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.nameless.onthego;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.SeekBar;
+
+import com.android.systemui.R;
+
+public class OnTheGoDialog extends Dialog {
+
+    protected final Context mContext;
+    protected final Handler mHandler = new Handler();
+
+    private final int mOnTheGoDialogLongTimeout;
+    private final int mOnTheGoDialogShortTimeout;
+
+    private final Runnable mDismissDialogRunnable = new Runnable() {
+        public void run() {
+            if (OnTheGoDialog.this.isShowing()) {
+                OnTheGoDialog.this.dismiss();
+            }
+        }
+
+        ;
+    };
+
+    public OnTheGoDialog(Context ctx) {
+        super(ctx);
+        mContext = ctx;
+        final Resources r = mContext.getResources();
+        mOnTheGoDialogLongTimeout =
+                r.getInteger(R.integer.quick_settings_brightness_dialog_long_timeout);
+        mOnTheGoDialogShortTimeout =
+                r.getInteger(R.integer.quick_settings_brightness_dialog_short_timeout);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setType(WindowManager.LayoutParams.TYPE_VOLUME_OVERLAY);
+        window.getAttributes().privateFlags |=
+                WindowManager.LayoutParams.PRIVATE_FLAG_SHOW_FOR_ALL_USERS;
+        window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        window.requestFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.quick_settings_onthego_dialog);
+        setCanceledOnTouchOutside(true);
+
+        final SeekBar mSlider = (SeekBar) findViewById(R.id.alpha_slider);
+        final float value = Settings.System.getFloat(mContext.getContentResolver(),
+                Settings.System.ON_THE_GO_ALPHA,
+                0.5f);
+        final int progress = ((int) (value * 100));
+        mSlider.setProgress(progress);
+        mSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                sendAlphaBroadcast(String.valueOf(i + 10));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                removeAllOnTheGoDialogCallbacks();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        dismissOnTheGoDialog(mOnTheGoDialogLongTimeout);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        removeAllOnTheGoDialogCallbacks();
+    }
+
+    private void dismissOnTheGoDialog(int timeout) {
+        removeAllOnTheGoDialogCallbacks();
+        mHandler.postDelayed(mDismissDialogRunnable, timeout);
+    }
+
+    private void removeAllOnTheGoDialogCallbacks() {
+        mHandler.removeCallbacks(mDismissDialogRunnable);
+    }
+
+    private void sendAlphaBroadcast(String i) {
+        final float value = (Float.parseFloat(i) / 100);
+        final Intent alphaBroadcast = new Intent();
+        alphaBroadcast.setAction(OnTheGoService.ACTION_TOGGLE_ALPHA);
+        alphaBroadcast.putExtra(OnTheGoService.EXTRA_ALPHA, value);
+        mContext.sendBroadcast(alphaBroadcast);
+    }
+
+}

TeamRevolution/frameworks_base

Conflicts:
	core/java/android/provider/Settings.java
	core/res/res/values/custom_symbols.xml
	core/res/res/values/ose_strings.xml
	packages/SystemUI/res/values/custom_strings.xml
	policy/src/com/android/internal/policy/impl/GlobalActions.java
	core/res/res/values/slim_strings.xml
	core/res/res/values/slim_symbols.xml
	packages/SystemUI/AndroidManifest.xml
	packages/SystemUI/res/values/slim_strings.xml

packages/SystemUI/src/com/android/systemui/ose/onthego/OnTheGoDialog.java
@@ -1,0 +1,126 @@
+/*
+ * Copyright (C) 2014 The OSE Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.ose.onthego;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.SeekBar;
+
+import com.android.systemui.R;
+
+public class OnTheGoDialog extends Dialog {
+
+    protected final Context mContext;
+    protected final Handler mHandler = new Handler();
+
+    private final int mOnTheGoDialogLongTimeout;
+    private final int mOnTheGoDialogShortTimeout;
+
+    private final Runnable mDismissDialogRunnable = new Runnable() {
+        public void run() {
+            if (OnTheGoDialog.this.isShowing()) {
+                OnTheGoDialog.this.dismiss();
+            }
+        }
+
+        ;
+    };
+
+    public OnTheGoDialog(Context ctx) {
+        super(ctx);
+        mContext = ctx;
+        final Resources r = mContext.getResources();
+        mOnTheGoDialogLongTimeout =
+                r.getInteger(R.integer.quick_settings_brightness_dialog_long_timeout);
+        mOnTheGoDialogShortTimeout =
+                r.getInteger(R.integer.quick_settings_brightness_dialog_short_timeout);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setType(WindowManager.LayoutParams.TYPE_VOLUME_OVERLAY);
+        window.getAttributes().privateFlags |=
+                WindowManager.LayoutParams.PRIVATE_FLAG_SHOW_FOR_ALL_USERS;
+        window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        window.requestFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.quick_settings_onthego_dialog);
+        setCanceledOnTouchOutside(true);
+
+        final SeekBar mSlider = (SeekBar) findViewById(R.id.alpha_slider);
+        final float value = Settings.System.getFloat(mContext.getContentResolver(),
+                Settings.System.ON_THE_GO_ALPHA,
+                0.5f);
+        final int progress = ((int) (value * 100));
+        mSlider.setProgress(progress);
+        mSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                sendAlphaBroadcast(String.valueOf(i + 10));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                removeAllOnTheGoDialogCallbacks();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        dismissOnTheGoDialog(mOnTheGoDialogLongTimeout);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        removeAllOnTheGoDialogCallbacks();
+    }
+
+    private void dismissOnTheGoDialog(int timeout) {
+        removeAllOnTheGoDialogCallbacks();
+        mHandler.postDelayed(mDismissDialogRunnable, timeout);
+    }
+
+    private void removeAllOnTheGoDialogCallbacks() {
+        mHandler.removeCallbacks(mDismissDialogRunnable);
+    }
+
+    private void sendAlphaBroadcast(String i) {
+        final float value = (Float.parseFloat(i) / 100);
+        final Intent alphaBroadcast = new Intent();
+        alphaBroadcast.setAction(OnTheGoService.ACTION_TOGGLE_ALPHA);
+        alphaBroadcast.putExtra(OnTheGoService.EXTRA_ALPHA, value);
+        mContext.sendBroadcast(alphaBroadcast);
+    }
+
+}