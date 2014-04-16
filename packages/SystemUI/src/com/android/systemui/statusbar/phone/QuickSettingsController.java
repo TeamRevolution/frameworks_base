a84e073453e662cc87f7fdc1319900843119b918
neophite31 <hard.d.25@gmail.com>
2014-04-16 13:17:06 +0200

    fix compilación ON THE GO!
    Change-Id: I7709107f3f3611c6ca4cf8222c882a0fc0ae7e55

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -54,7 +54,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_WIFI;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.slim.QSConstants.TILE_INTERNALMEMORY;
-import static com.android.internal.util.ose.QSConstants.TILE_ONTHEGO;
+import static com.android.internal.util.slim.QSConstants.TILE_ONTHEGO;
 
 import android.app.Activity;
 import android.app.ActivityManagerNative;
